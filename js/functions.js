02b9b0179ba8cd39966a7e84920e57d0e48beec8
Alfredo <jorgealfredopulido@gmail.com>
2016-09-12 23:14:52 -0500

    veamos

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,13 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    window.open = cordova.InAppBrowser.open;
+}
+
 $(function(){
 	
 
-	$("a").click(function(ev){
+	$("a.linkexterno").click(function(ev){
 		ev.preventDefault();
 		var ref = window.open($(this).attr("href"), '_blank', 'location=yes'); 
-		alert("open"+$(this).attr("href"));
-
 	});
 });
