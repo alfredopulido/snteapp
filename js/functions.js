c3246c8dc947145630d5e8cf87e6bc04c53d2933
Alfredo <jorgealfredopulido@gmail.com>
2016-09-12 22:50:46 -0500

    calando

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,13 @@ $(function(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 	function onDeviceReady() {
 	    window.open = cordova.InAppBrowser.open;
+	    alert("device ready");
 	}
 
 	$("a").click(function(ev){
 		ev.preventDefault();
 		var ref = window.open($(this).attr("href"), '_blank', 'location=yes'); 
+		alert("open"+$(this).attr("href"));
 
 	});
 });
